b15a85930e3070d94d5bc7ab604389504077e50c
mfasquel <fasquel.m@gmail.com>
2019-08-16 23:04:36 +0200

    Apply suggestions from code review
    Co-Authored-By: Wim Goeman <4943756+wimgoeman@users.noreply.github.com>

--- src/main/kotlin/fsrss/FeedController.kt
+++ src/main/kotlin/fsrss/FeedController.kt
@@ -3,61 +3,55 @@ package fsrss
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.client.HttpClientErrorException
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 
 
 @Controller
 class FeedController {
-    val path: String = System.getenv("RSS_FEEDS_PATH") ?: "/"
+    val path: String = System.getenv("RSS_FEEDS_PATH") ?: "/data"
     val jsonMapper = jacksonObjectMapper()
 
     @GetMapping("/feeds/{feed}")
-    @ResponseBody
-    fun readFeed(@PathVariable feed: String) : String {
+    fun readFeed(@PathVariable feed: String) : ResponseEntity<String> {
         val path = Path.of(path, feed)
-        if (Files.exists(path)) {
-            return FeedHandler().createRssFeed(path.toFile())
-        } else {
-            throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Feed does not exist")
-        }
-    }
-
-    @RequestMapping("/feeds/{feed}")
-    @ResponseStatus(HttpStatus.OK)
-    fun createFeed(@PathVariable feed: String, @RequestBody body: String) {
-        val path = Path.of(path + feed)
         if (!Files.exists(path)) {
-            val meta: FeedMetaData = jsonMapper.readValue(body)
-            Files.createDirectory(path)
-            val metaFile = File("$path/meta.json")
-            metaFile.createNewFile()
-            metaFile.writeText(jsonMapper.writeValueAsString(meta))
-        } else {
-            throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Feed already exist")
+            return ResponseEntity("Feed does not exist", HttpStatus.BAD_REQUEST)
         }
+
+        return ResponseEntity(FeedHandler().createRssFeed(path.toFile()), HttpStatus.OK)
     }
 
-    @RequestMapping("/feeds/{feed}/item")
-    @ResponseStatus(HttpStatus.OK)
-    fun addItem(@PathVariable feed:String, @RequestBody body: String) {
+    @PostMapping("/feeds/{feed}")
+    fun createFeed(@PathVariable feed: String, @RequestBody body: String): ResponseEntity<String> {
         val path = Path.of(path, feed)
         if (Files.exists(path)) {
-            val item: Item = jsonMapper.readValue(body)
-            val sanitizedTitle: String = item.title.filter{it.isLetterOrDigit()}.toLowerCase()
-            val itemFile = File(path.toFile(), sanitizedTitle)
-            if (!itemFile.exists())  {
-                itemFile.createNewFile()
-                itemFile.writeText(jsonMapper.writeValueAsString(item))
-            } else {
-                throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Item already exist")
-            }
-        } else {
-            throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Feed does not exist")
+            return ResponseEntity("Feed already exist", HttpStatus.BAD_REQUEST)
         }
+        val meta: FeedMetaData = jsonMapper.readValue(body)
+        Files.createDirectory(path)
+        val metaFile = Files.createFile(Path.of(path.toString(), "meta.json"))
+        Files.writeString(metaFile, jsonMapper.writeValueAsString(meta))
+        return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @PostMapping("/feeds/{feed}/item")
+    fun addItem(@PathVariable feed:String, @RequestBody body: String): ResponseEntity<String> {
+        val path = Path.of(path, feed)
+        if (!Files.exists(path)) {
+            return ResponseEntity("Feed does not exist", HttpStatus.BAD_REQUEST)
+        }
+        val item: Item = jsonMapper.readValue(body)
+        val sanitizedTitle: String = item.title.filter{it.isLetterOrDigit()}.toLowerCase()
+        val itemFile = Path.of(path.toString(), sanitizedTitle)
+        if (Files.exists(itemFile))  {
+            return ResponseEntity("Item already exist", HttpStatus.BAD_REQUEST)
+        }
+
+        Files.writeString(itemFile, jsonMapper.writeValueAsString(item))
+        return ResponseEntity(HttpStatus.CREATED)
     }
 }
